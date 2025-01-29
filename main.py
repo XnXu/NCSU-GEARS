d9682daaf0d5aa599fcba6a129ca4b2e81b467f1
AIAI333 <12312107@mail.sustech.edu.cn>
2025-01-30 04:54:58 +0800

    Update reward function, model architecture and the default load model

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.vec_env import DummyVecEnv
 import numpy as np
 from stable_baselines3.common.utils import set_random_seed
+import torch
+from stable_baselines3.td3.policies import TD3Policy
+import matplotlib.pyplot as plt
 
 # Setting up the argument parser for command-line inputs
 parser = argparse.ArgumentParser()
@@ -28,7 +31,7 @@ parser.add_argument('--train_timesteps', type=int, default=None, help='Number of
 # Maximum steps allowed per episode
 parser.add_argument('--max_episode_step', type=int, default=None, help='Maximum steps allowed per episode.')
 
-parser.add_argument('--seed', type=int, default=4, help='Random seed to guarantee reproducibility')
+parser.add_argument('--seed', type=int, default=3, help='Random seed to guarantee reproducibility')
 #seed 3
 # Parse the input arguments
 args = parser.parse_args()
@@ -70,10 +73,30 @@ else:
     )
 
     # Initialize a new TD3 model with a custom neural network architecture
+    class ScaledTD3Policy(TD3Policy):
+        def __init__(self, *args, **kwargs):
+            super(ScaledTD3Policy, self).__init__(*args, **kwargs)
+
+        def forward(self, obs, deterministic=False):
+            # Get actions from the parent class (TD3Policy)
+            actions = super().forward(obs, deterministic)
+            # Scale the actions by (1 / pi)
+            scaled_actions = actions
+            return scaled_actions
+
+        def _predict(self, obs, deterministic=False):
+            # Get actions using the base TD3 policy
+            actions = super()._predict(obs, deterministic)
+            # Scale the actions by (1 / pi)
+            scaled_actions = actions
+            return scaled_actions
+
+
+    # Create the TD3 model with the custom policy
     model = TD3(
-        'MlpPolicy',
+        ScaledTD3Policy,
         env,
-        policy_kwargs=dict(net_arch=[128, 256, 32]),
+        policy_kwargs=dict(net_arch=[256, 256, 32]),
         action_noise=action_noise
     )
 
@@ -92,22 +115,47 @@ if eval_episodes is not None:
         done = False
         # Accumulate rewards for this episode
         total_reward = 0
-        while not done:
+        force_list = []
+        cos_list = []
+        x_dot_list = []
+        theta_dot_list = []
+        i = 0
+        while not done and i < 1000:
+            i += 1
             # Predict the action
             action, _ = model.predict(obs, deterministic=False)
+            force_list.append(10 * action[0])
             # Take the action and observe the result
             obs, reward, done, info = env.step(action)
+            cos_list.append(obs[0][2])
+            x_dot_list.append(obs[0][1])
+            theta_dot_list.append(obs[0][4])
             # Add the reward to the total
             total_reward += reward
             # Render the environment
             env.render()
         print(f'Episode: {episode + 1} | Total Reward: {total_reward}')
+        fig, axes = plt.subplots(4, 1, figsize=(8, 12))  # 4 rows, 1 column
 
+        axes[0].plot(cos_list)
+        axes[0].set_ylabel('Cosine theta')
+
+        axes[1].plot(x_dot_list)
+        axes[1].set_ylabel('x_dot')
+
+        axes[2].plot(theta_dot_list)
+        axes[2].set_ylabel('theta_dot')
+
+        axes[3].plot(force_list)
+        axes[3].set_ylabel('force')
+
+        plt.tight_layout()  # Adjusts layout to prevent overlapping labels
+        plt.show()
 # Save the trained model if a save path is specified
 if save:
     print(f"Saving the model")
-    model.save('model/td3/td3_swingup_balance')
-    model.save_replay_buffer("model/td3/td3_swingup_balance_replay_buffer")
+    model.save('model/td3_smaller_force/td3_swingup_balance')
+    model.save_replay_buffer("model/td3_smaller_force/td3_swingup_balance_replay_buffer")
 
 # Close the environment
 env.close()
